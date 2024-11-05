3d39e0d1121bf14d37b057064490dc8eb7883a7b
achrefmechergui <achreffcb123achref@gamil.com>
2024-11-05 21:00:14 +0100

    done

--- app.js
+++ app.js
@@ -2,18 +2,46 @@ const closeButton = document.querySelector(".close-btn")
 const overlay = document.querySelector(".overlay")
 const modal = document.querySelector(".modal")
 const showModal = document.querySelectorAll(".show-modal")
-console.log(showModal)
+// console.log(showModal)
 
-const displayModal = function (){
-   for (let i = 0 ; i < showModal.length ; i++){
-    showModal[i].addEventListener("click",function(){
+// const displayModal = function(){
+//     modal.classList.remove("hidden") 
+//     overlay.classList.remove("hidden")
+// }
+
+      function displayModal (){ ///// function declaration
         modal.classList.remove("hidden") 
         overlay.classList.remove("hidden")
-    })
-   }
+    }
+
+
+
+const hideModal = function (){  ///// function expression
+    modal.classList.add("hidden") 
+    overlay.classList.add("hidden")
 }
 
-displayModal()
+for (let i = 0 ; i < showModal.length ; i++){
+    showModal[i].addEventListener("click",displayModal)
+   }  
+
+
+  closeButton.addEventListener("click",hideModal)
+
+  overlay.addEventListener("click",hideModal)
+
+
+  document.addEventListener("keydown",function(e){
+     console.log(e.key)
+     if(e.key==="Escape"){
+        hideModal()
+     }
+  })
+
+//   function hello (){
+//     console.log("hello")
+//   }
+/// hello()
 
 
 
